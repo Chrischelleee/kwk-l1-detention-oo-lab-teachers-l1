5ea2ffea452640e3803f2d4fb7ff396e5fa9a56b
Chrischelle Acelajado <chrischelleacelajado@gmail.com>
2018-07-26 19:09:34 +0000

    automatically backed up by learn

--- detention.rb
+++ detention.rb
@@ -1,5 +1,6 @@
 #Code your detention class here
 class Detention
+  attr_accessor :activity, :location, :time, :grumpy_teacher
   def initialize
     @activity = "collective punishment"
     @students = []
@@ -16,34 +17,5 @@ class Detention
   def remove_student(students)
     @students.delete(students)
   end
-  
-  def activity
-    @activity
-  end
-
-  def location=(location)
-    @location = location
-  end
-
-  def location
-    @location
-  end
-
-  def time=(num)
-    @time = num
-  end
-
-  def time
-    @time
-  end
-
-  def grumpy_teacher=(grumpy_teacher)
-    @grumpy_teacher = grumpy_teacher
-  end
-
-  def grumpy_teacher
-    @grumpy_teacher
-  end
-
 
 end
